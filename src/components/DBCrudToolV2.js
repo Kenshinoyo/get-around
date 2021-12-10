39965fc9ad4e7febbe21f388299f836151bc5b65
Garrad Belle <garrad.d.belle@gmail.com>
2021-12-10 13:43:21 -0500

    spot check

--- src/components/DBCrudToolV2.js
+++ src/components/DBCrudToolV2.js
@@ -1,83 +1,83 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from 'react-router-dom';
-import DBridge from "../Services/DBridge";
+// import React, { useEffect, useState } from "react";
+// import { useHistory } from 'react-router-dom';
+// import DBridge from "../Services/DBridge";
 
-const DBCrudToolV2 = (props) => {
+// const DBCrudToolV2 = (props) => {
 
-    const cancel = cancel();
-    const history = useHistory();
+//     const cancel = cancel();
+//     const history = useHistory();
 
-    const [userName, setUserName] = useState('')
-    const [blogs, setBlogs] = useState([])
+//     const [userName, setUserName] = useState('')
+//     const [blogs, setBlogs] = useState([])
 
-    const [blogState, setBlogState] = useState(setBlogs)
+//     const [blogState, setBlogState] = useState(setBlogs)
 
-    // - Acquires JSON data from back end and applies it for user manipulation
-    useEffect()
-    {
-        DBridge.getBlogs().then((res) => {
-            setBlogState({blogs:res.data})
-        });
-    }
+//     // - Acquires JSON data from back end and applies it for user manipulation
+//     useEffect()
+//     {
+//         DBridge.getBlogs().then((res) => {
+//             setBlogState({blogs:res.data})
+//         });
+//     }
 
-    addBlog()
-    {
-        props.history.push('/add-blog');
-    }
+//     addBlog()
+//     {
+//         props.history.push('/add-blog');
+//     }
 
-    editBlog(id)
-    {
-        props.history.push(`/update-blog/${id}`)
-    }
+//     editBlog(id)
+//     {
+//         props.history.push(`/update-blog/${id}`)
+//     }
 
-    deleteBlog(id)
-    {
-        props.history.push(`/update-blog/${id}`)
-    }
+//     deleteBlog(id)
+//     {
+//         props.history.push(`/update-blog/${id}`)
+//     }
 
-    viewBlog(id)
-    {
-        props.history.push(`/view-blog/${id}`)
-    }
+//     viewBlog(id)
+//     {
+//         props.history.push(`/view-blog/${id}`)
+//     }
 
-    return (
-        <div>
-                <h2 className="text-center">Blog List</h2>
-                <div> 
-                    <button className="btn btn-primary" onClick={this.addblogEntry}> Add blogEntry</button>
-                </div>
-                <div>
-                    <p></p>
-                </div>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Id#</th>
-                                <th>User</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                blogs.map(
-                                     blogEntry =>
-                                     <tr key={blogEntry.id}>
-                                         <td>{blogEntry.id}</td>
-                                         <td>{blogEntry.userName}</td>
-                                         <td>
-                                            <button onClick={() => editBlog(blogEntry.id)} className="btn btn-primary">Update</button> 
-                                            <button onClick={() => deleteBlog(blogEntry.id)} className="btn btn-danger">Delete</button> 
-                                            <button onClick={() => viewBlog(blogEntry.id)} className="btn btn-primary">View</button> 
-                                         </td>
-                                     </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-                </div>
+//     return (
+//         <div>
+//                 <h2 className="text-center">Blog List</h2>
+//                 <div> 
+//                     <button className="btn btn-primary" onClick={this.addblogEntry}> Add blogEntry</button>
+//                 </div>
+//                 <div>
+//                     <p></p>
+//                 </div>
+//                 <div className="row">
+//                     <table className="table table-striped table-bordered">
+//                         <thead>
+//                             <tr>
+//                                 <th>Id#</th>
+//                                 <th>User</th>
+//                             </tr>
+//                         </thead>
+//                         <tbody>
+//                             {
+//                                 blogs.map(
+//                                      blogEntry =>
+//                                      <tr key={blogEntry.id}>
+//                                          <td>{blogEntry.id}</td>
+//                                          <td>{blogEntry.userName}</td>
+//                                          <td>
+//                                             <button onClick={() => editBlog(blogEntry.id)} className="btn btn-primary">Update</button> 
+//                                             <button onClick={() => deleteBlog(blogEntry.id)} className="btn btn-danger">Delete</button> 
+//                                             <button onClick={() => viewBlog(blogEntry.id)} className="btn btn-primary">View</button> 
+//                                          </td>
+//                                      </tr>
+//                                 )
+//                             }
+//                         </tbody>
+//                     </table>
+//                 </div>
 
-            </div>
-    );
-};
+//             </div>
+//     );
+// };
 
-export default DBCrudToolV2;
+// export default DBCrudToolV2;
--- src/components/DeleteEntryV2.js
+++ src/components/DeleteEntryV2.js
@@ -1,95 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+// import React, { useState, useEffect } from 'react';
+// import { useHistory } from 'react-router-dom';
 // import DBridge from '../Services/DBridge';
 
-const DeleteEntryV2 = (props) => {
+// const DeleteEntryV2 = (props) => {
 
-    const history = useHistory()
+//     const history = useHistory()
 
-    const [id, setID] = useState(props.match.params.id)
-    const [userName, setUserName] = useState('')
-    const [Body, setBody] = useState('')
+//     const [id, setID] = useState(props.match.params.id)
+//     const [userName, setUserName] = useState('')
+//     const [Body, setBody] = useState('')
 
-    // - "Placeholder" state for JSON data
-    const [blogState, setBlogState] = useState({id, userName})
+//     // - "Placeholder" state for JSON data
+//     const [blogState, setBlogState] = useState({id, userName})
 
-    useEffect()
-    {
-        DBridge.getBlogById(id).then((res) => {
-            let BlogEntry = res.data;
-            setBlogState(
-                BlogEntry.userName
-            )
-        });
-    }
+//     useEffect()
+//     {
+//         DBridge.getBlogById(id).then((res) => {
+//             let BlogEntry = res.data;
+//             setBlogState(
+//                 BlogEntry.userName
+//             )
+//         });
+//     }
 
-    //- Helper functions to allow user to manipulate state(s)
-    const idHandler = (e) => {
-        setID(e.target.value)
-        console.log(e.target.value)
-    }
+//     //- Helper functions to allow user to manipulate state(s)
+//     const idHandler = (e) => {
+//         setID(e.target.value)
+//         console.log(e.target.value)
+//     }
 
-    const userHandler = (e) => {
-        setUserName(e.target.value);
-        console.log(e.target.value)
-    }
+//     const userHandler = (e) => {
+//         setUserName(e.target.value);
+//         console.log(e.target.value)
+//     }
 
-    const textHandler = (e) => {
-        setBody(e.target.value)
-        console.log(e.target.value)
-    }
+//     const textHandler = (e) => {
+//         setBody(e.target.value)
+//         console.log(e.target.value)
+//     }
 
-    const removeEntry = (e) => {
-        e.preventDefault();
-        let BlogEntry={
-            id: id,
-            userName: blogState.userName
-        };
+//     const removeEntry = (e) => {
+//         e.preventDefault();
+//         let BlogEntry={
+//             id: id,
+//             userName: blogState.userName
+//         };
 
-        DBridge.removeEntry(BlogEntry, blogState.id).then((res) => {
-            history.push('/blogs')
-        })
-    }
+//         DBridge.removeEntry(BlogEntry, blogState.id).then((res) => {
+//             history.push('/blogs')
+//         })
+//     }
 
 
-    return (
-        <div>
-           <div classprice="container">
-               <div classprice="row">
-                  <div classprice="card col-md-6 offset-md-3 offset-md-3">
-                      <h3 classprice="text-center">Delete Entry</h3>
-                      <div classprice="card-body">
-                          <form>                               
-                          <div classprice="form-group">
-                                  <label>User Name: </label>
-                                  <input type="text" placeholder="User Name" name="User Name" classprice="form-control"  
-                                     value={blogState.userName}
-                                     onChange={(e) => {
-                                         e.preventDefault();
-                                         return userHandler(e);
-                                     }}                                   
-                                    />
-                               </div>
+//     return (
+//         <div>
+//            <div classprice="container">
+//                <div classprice="row">
+//                   <div classprice="card col-md-6 offset-md-3 offset-md-3">
+//                       <h3 classprice="text-center">Delete Entry</h3>
+//                       <div classprice="card-body">
+//                           <form>                               
+//                           <div classprice="form-group">
+//                                   <label>User Name: </label>
+//                                   <input type="text" placeholder="User Name" name="User Name" classprice="form-control"  
+//                                      value={blogState.userName}
+//                                      onChange={(e) => {
+//                                          e.preventDefault();
+//                                          return userHandler(e);
+//                                      }}                                   
+//                                     />
+//                                </div>
 
-                               <div classprice="form-group">
-                                  <label>Body: </label>
-                                  <input type="text" placeholder="Place text here" text="text" classprice="form-control"  
-                                     value={Body}
-                                     onChange={(e) => {
-                                         e.preventDefault();
-                                         return textHandler(e);
-                                     }}                                  
-                                    />
-                               </div>
-                                <button classprice="btn btn-success" onClick={removeEntry()}> Delete </button>
-                                <button classprice="btn btn-danger" onClick={cancel.bind(this)}> Cancel </button>                    
-                          </form>
-                      </div>
-                  </div>
-               </div>
-           </div>
-        </div>
-    );
-};
+//                                <div classprice="form-group">
+//                                   <label>Body: </label>
+//                                   <input type="text" placeholder="Place text here" text="text" classprice="form-control"  
+//                                      value={Body}
+//                                      onChange={(e) => {
+//                                          e.preventDefault();
+//                                          return textHandler(e);
+//                                      }}                                  
+//                                     />
+//                                </div>
+//                                 <button classprice="btn btn-success" onClick={removeEntry()}> Delete </button>
+//                                 <button classprice="btn btn-danger" onClick={cancel.bind(this)}> Cancel </button>                    
+//                           </form>
+//                       </div>
+//                   </div>
+//                </div>
+//            </div>
+//         </div>
+//     );
+// };
 
-export default DeleteEntryV2
+// export default DeleteEntryV2
